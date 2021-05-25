a90c7119fc071450cbbcdc8b382d051a569770bc
Isaak <isaak.hanimann@gmail.com>
2021-05-25 12:41:56 +0200

    Add pickVertex()

--- assignment6/src/main.cpp
+++ assignment6/src/main.cpp
@@ -3,6 +3,7 @@
 #include <igl/opengl/glfw/imgui/ImGuiMenu.h>
 #include <imgui/imgui.h>
 #include <vector>
+#include <igl/unproject_onto_mesh.h>
 
 using namespace std;
 using namespace Eigen;
@@ -15,6 +16,35 @@ Eigen::MatrixXd V(0,3);
 //face array, #F x3
 Eigen::MatrixXi F(0,3);
 
+
+int pickVertex(int mouse_x, int mouse_y) {
+    int vi = -1;
+
+    int fid;
+    Eigen::Vector3f bc;
+    // Cast a ray in the view direction starting from the mouse position
+    double x = viewer.current_mouse_x;
+    double y = viewer.core.viewport(3) - viewer.current_mouse_y;
+    if(igl::unproject_onto_mesh(Eigen::Vector2f(x,y), viewer.core.view /* viewer.data().model*/,
+                                viewer.core.proj, viewer.core.viewport, V, F, fid, bc)) {
+        // paint hit red
+        bc.maxCoeff(&vi);
+        vi = F(fid,vi);
+    }
+    return vi;
+
+}
+
+bool callback_mouse_down(Viewer& viewer, int button, int modifier)
+{
+    if (button == (int) Viewer::MouseButton::Right)
+        return false;
+
+    int vi = pickVertex(viewer.current_mouse_x, viewer.current_mouse_y);
+    cout << "Found vertex with index: " << vi << endl;
+
+    return true;
+}
 
 bool callback_key_down(Viewer& viewer, unsigned char key, int modifiers) {
     if (key == '1') {
@@ -58,6 +88,7 @@ int main(int argc, char *argv[]) {
   };
 
   viewer.callback_key_down = callback_key_down;
+  //viewer.callback_mouse_down = callback_mouse_down;
 
   viewer.data().point_size = 10;
   viewer.core.set_rotation_type(igl::opengl::ViewerCore::ROTATION_TYPE_TRACKBALL);
